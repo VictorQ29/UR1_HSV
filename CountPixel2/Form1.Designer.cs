be9c19b38fcfb7c0cd3d3cee312039412f99b20a
VictorQ29 <98054563+VictorQ29@users.noreply.github.com>
2022-02-21 15:33:14 -0500

    Add files via upload

---
+++ CountPixel2/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace CountPixel2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emguPictureBox = new System.Windows.Forms.PictureBox();
+            this.binaryPictureBox = new System.Windows.Forms.PictureBox();
+            this.thresholdTrackBar = new System.Windows.Forms.TrackBar();
+            this.PixelCountLabel1 = new System.Windows.Forms.Label();
+            this.PixelCountLabel2 = new System.Windows.Forms.Label();
+            this.PixelCountLabel3 = new System.Windows.Forms.Label();
+            this.PixelCountLabel4 = new System.Windows.Forms.Label();
+            this.PixelCountLabel5 = new System.Windows.Forms.Label();
+            this.PixelCountLabel6 = new System.Windows.Forms.Label();
+            this.PixelCountLabel7 = new System.Windows.Forms.Label();
+            this.SaturationPictureBox = new System.Windows.Forms.PictureBox();
+            this.ValuePictureBox = new System.Windows.Forms.PictureBox();
+            this.HuePictureBox = new System.Windows.Forms.PictureBox();
+            this.RedLinePictureBox = new System.Windows.Forms.PictureBox();
+            this.HueTrackBar1 = new System.Windows.Forms.TrackBar();
+            this.HueTrackBar2 = new System.Windows.Forms.TrackBar();
+            this.SaturationTrackBar1 = new System.Windows.Forms.TrackBar();
+            this.SaturationTrackBar2 = new System.Windows.Forms.TrackBar();
+            this.ValueTrackBar1 = new System.Windows.Forms.TrackBar();
+            this.ValueTrackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValuePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HuePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedLinePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValueTrackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValueTrackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // emguPictureBox
+            // 
+            this.emguPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.emguPictureBox.Name = "emguPictureBox";
+            this.emguPictureBox.Size = new System.Drawing.Size(303, 225);
+            this.emguPictureBox.TabIndex = 0;
+            this.emguPictureBox.TabStop = false;
+            this.emguPictureBox.UseWaitCursor = true;
+            // 
+            // binaryPictureBox
+            // 
+            this.binaryPictureBox.Location = new System.Drawing.Point(321, 9);
+            this.binaryPictureBox.Name = "binaryPictureBox";
+            this.binaryPictureBox.Size = new System.Drawing.Size(301, 225);
+            this.binaryPictureBox.TabIndex = 1;
+            this.binaryPictureBox.TabStop = false;
+            this.binaryPictureBox.UseWaitCursor = true;
+            // 
+            // thresholdTrackBar
+            // 
+            this.thresholdTrackBar.Location = new System.Drawing.Point(309, 256);
+            this.thresholdTrackBar.Maximum = 255;
+            this.thresholdTrackBar.Name = "thresholdTrackBar";
+            this.thresholdTrackBar.Size = new System.Drawing.Size(313, 56);
+            this.thresholdTrackBar.TabIndex = 2;
+            this.thresholdTrackBar.UseWaitCursor = true;
+            this.thresholdTrackBar.Scroll += new System.EventHandler(this.thresholdTrackBar_Scroll);
+            // 
+            // PixelCountLabel1
+            // 
+            this.PixelCountLabel1.AutoSize = true;
+            this.PixelCountLabel1.Location = new System.Drawing.Point(308, 236);
+            this.PixelCountLabel1.Name = "PixelCountLabel1";
+            this.PixelCountLabel1.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel1.TabIndex = 3;
+            this.PixelCountLabel1.Text = "1";
+            this.PixelCountLabel1.UseWaitCursor = true;
+            // 
+            // PixelCountLabel2
+            // 
+            this.PixelCountLabel2.AutoSize = true;
+            this.PixelCountLabel2.Location = new System.Drawing.Point(363, 236);
+            this.PixelCountLabel2.Name = "PixelCountLabel2";
+            this.PixelCountLabel2.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel2.TabIndex = 4;
+            this.PixelCountLabel2.Text = "2";
+            this.PixelCountLabel2.UseWaitCursor = true;
+            // 
+            // PixelCountLabel3
+            // 
+            this.PixelCountLabel3.AutoSize = true;
+            this.PixelCountLabel3.Location = new System.Drawing.Point(417, 236);
+            this.PixelCountLabel3.Name = "PixelCountLabel3";
+            this.PixelCountLabel3.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel3.TabIndex = 5;
+            this.PixelCountLabel3.Text = "3";
+            this.PixelCountLabel3.UseWaitCursor = true;
+            // 
+            // PixelCountLabel4
+            // 
+            this.PixelCountLabel4.AutoSize = true;
+            this.PixelCountLabel4.Location = new System.Drawing.Point(464, 236);
+            this.PixelCountLabel4.Name = "PixelCountLabel4";
+            this.PixelCountLabel4.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel4.TabIndex = 6;
+            this.PixelCountLabel4.Text = "4";
+            this.PixelCountLabel4.UseWaitCursor = true;
+            // 
+            // PixelCountLabel5
+            // 
+            this.PixelCountLabel5.AutoSize = true;
+            this.PixelCountLabel5.Location = new System.Drawing.Point(517, 236);
+            this.PixelCountLabel5.Name = "PixelCountLabel5";
+            this.PixelCountLabel5.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel5.TabIndex = 7;
+            this.PixelCountLabel5.Text = "5";
+            this.PixelCountLabel5.UseWaitCursor = true;
+            // 
+            // PixelCountLabel6
+            // 
+            this.PixelCountLabel6.AutoSize = true;
+            this.PixelCountLabel6.Location = new System.Drawing.Point(564, 236);
+            this.PixelCountLabel6.Name = "PixelCountLabel6";
+            this.PixelCountLabel6.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel6.TabIndex = 8;
+            this.PixelCountLabel6.Text = "6";
+            this.PixelCountLabel6.UseWaitCursor = true;
+            // 
+            // PixelCountLabel7
+            // 
+            this.PixelCountLabel7.AutoSize = true;
+            this.PixelCountLabel7.Location = new System.Drawing.Point(606, 236);
+            this.PixelCountLabel7.Name = "PixelCountLabel7";
+            this.PixelCountLabel7.Size = new System.Drawing.Size(16, 17);
+            this.PixelCountLabel7.TabIndex = 9;
+            this.PixelCountLabel7.Text = "7";
+            this.PixelCountLabel7.UseWaitCursor = true;
+            // 
+            // SaturationPictureBox
+            // 
+            this.SaturationPictureBox.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.SaturationPictureBox.Location = new System.Drawing.Point(915, 12);
+            this.SaturationPictureBox.Name = "SaturationPictureBox";
+            this.SaturationPictureBox.Size = new System.Drawing.Size(257, 198);
+            this.SaturationPictureBox.TabIndex = 10;
+            this.SaturationPictureBox.TabStop = false;
+            // 
+            // ValuePictureBox
+            // 
+            this.ValuePictureBox.Location = new System.Drawing.Point(1189, 12);
+            this.ValuePictureBox.Name = "ValuePictureBox";
+            this.ValuePictureBox.Size = new System.Drawing.Size(247, 198);
+            this.ValuePictureBox.TabIndex = 11;
+            this.ValuePictureBox.TabStop = false;
+            // 
+            // HuePictureBox
+            // 
+            this.HuePictureBox.Location = new System.Drawing.Point(646, 12);
+            this.HuePictureBox.Name = "HuePictureBox";
+            this.HuePictureBox.Size = new System.Drawing.Size(249, 198);
+            this.HuePictureBox.TabIndex = 12;
+            this.HuePictureBox.TabStop = false;
+            // 
+            // RedLinePictureBox
+            // 
+            this.RedLinePictureBox.Location = new System.Drawing.Point(12, 318);
+            this.RedLinePictureBox.Name = "RedLinePictureBox";
+            this.RedLinePictureBox.Size = new System.Drawing.Size(312, 230);
+            this.RedLinePictureBox.TabIndex = 13;
+            this.RedLinePictureBox.TabStop = false;
+            // 
+            // HueTrackBar1
+            // 
+            this.HueTrackBar1.Location = new System.Drawing.Point(646, 225);
+            this.HueTrackBar1.Maximum = 255;
+            this.HueTrackBar1.Name = "HueTrackBar1";
+            this.HueTrackBar1.Size = new System.Drawing.Size(249, 56);
+            this.HueTrackBar1.TabIndex = 14;
+            this.HueTrackBar1.UseWaitCursor = true;
+            this.HueTrackBar1.Scroll += new System.EventHandler(this.HueTrackBar1_Scroll);
+            // 
+            // HueTrackBar2
+            // 
+            this.HueTrackBar2.Location = new System.Drawing.Point(646, 271);
+            this.HueTrackBar2.Maximum = 255;
+            this.HueTrackBar2.Name = "HueTrackBar2";
+            this.HueTrackBar2.Size = new System.Drawing.Size(249, 56);
+            this.HueTrackBar2.TabIndex = 15;
+            this.HueTrackBar2.UseWaitCursor = true;
+            this.HueTrackBar2.Scroll += new System.EventHandler(this.HueTrackBar2_Scroll);
+            // 
+            // SaturationTrackBar1
+            // 
+            this.SaturationTrackBar1.Location = new System.Drawing.Point(923, 225);
+            this.SaturationTrackBar1.Maximum = 255;
+            this.SaturationTrackBar1.Name = "SaturationTrackBar1";
+            this.SaturationTrackBar1.Size = new System.Drawing.Size(249, 56);
+            this.SaturationTrackBar1.TabIndex = 16;
+            this.SaturationTrackBar1.UseWaitCursor = true;
+            this.SaturationTrackBar1.Scroll += new System.EventHandler(this.SaturationTrackBar1_Scroll);
+            // 
+            // SaturationTrackBar2
+            // 
+            this.SaturationTrackBar2.Location = new System.Drawing.Point(923, 271);
+            this.SaturationTrackBar2.Maximum = 255;
+            this.SaturationTrackBar2.Name = "SaturationTrackBar2";
+            this.SaturationTrackBar2.Size = new System.Drawing.Size(249, 56);
+            this.SaturationTrackBar2.TabIndex = 17;
+            this.SaturationTrackBar2.UseWaitCursor = true;
+            this.SaturationTrackBar2.Scroll += new System.EventHandler(this.SaturationTrackBar2_Scroll);
+            // 
+            // ValueTrackBar1
+            // 
+            this.ValueTrackBar1.Location = new System.Drawing.Point(1189, 225);
+            this.ValueTrackBar1.Maximum = 255;
+            this.ValueTrackBar1.Name = "ValueTrackBar1";
+            this.ValueTrackBar1.Size = new System.Drawing.Size(249, 56);
+            this.ValueTrackBar1.TabIndex = 18;
+            this.ValueTrackBar1.UseWaitCursor = true;
+            this.ValueTrackBar1.Scroll += new System.EventHandler(this.ValueTrackBar1_Scroll);
+            // 
+            // ValueTrackBar2
+            // 
+            this.ValueTrackBar2.Location = new System.Drawing.Point(1189, 271);
+            this.ValueTrackBar2.Maximum = 255;
+            this.ValueTrackBar2.Name = "ValueTrackBar2";
+            this.ValueTrackBar2.Size = new System.Drawing.Size(249, 56);
+            this.ValueTrackBar2.TabIndex = 19;
+            this.ValueTrackBar2.UseWaitCursor = true;
+            this.ValueTrackBar2.Scroll += new System.EventHandler(this.ValueTrackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1438, 653);
+            this.Controls.Add(this.ValueTrackBar2);
+            this.Controls.Add(this.ValueTrackBar1);
+            this.Controls.Add(this.SaturationTrackBar2);
+            this.Controls.Add(this.SaturationTrackBar1);
+            this.Controls.Add(this.HueTrackBar2);
+            this.Controls.Add(this.HueTrackBar1);
+            this.Controls.Add(this.RedLinePictureBox);
+            this.Controls.Add(this.HuePictureBox);
+            this.Controls.Add(this.ValuePictureBox);
+            this.Controls.Add(this.SaturationPictureBox);
+            this.Controls.Add(this.PixelCountLabel7);
+            this.Controls.Add(this.PixelCountLabel6);
+            this.Controls.Add(this.PixelCountLabel5);
+            this.Controls.Add(this.PixelCountLabel4);
+            this.Controls.Add(this.PixelCountLabel3);
+            this.Controls.Add(this.PixelCountLabel2);
+            this.Controls.Add(this.PixelCountLabel1);
+            this.Controls.Add(this.thresholdTrackBar);
+            this.Controls.Add(this.binaryPictureBox);
+            this.Controls.Add(this.emguPictureBox);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValuePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HuePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedLinePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValueTrackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ValueTrackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private System.Windows.Forms.PictureBox binaryPictureBox;
+        private System.Windows.Forms.TrackBar thresholdTrackBar;
+        private System.Windows.Forms.Label PixelCountLabel1;
+        private System.Windows.Forms.Label PixelCountLabel2;
+        private System.Windows.Forms.Label PixelCountLabel3;
+        private System.Windows.Forms.Label PixelCountLabel4;
+        private System.Windows.Forms.Label PixelCountLabel5;
+        private System.Windows.Forms.Label PixelCountLabel6;
+        private System.Windows.Forms.Label PixelCountLabel7;
+        private System.Windows.Forms.PictureBox SaturationPictureBox;
+        private System.Windows.Forms.PictureBox ValuePictureBox;
+        private System.Windows.Forms.PictureBox HuePictureBox;
+        private System.Windows.Forms.PictureBox RedLinePictureBox;
+        private System.Windows.Forms.TrackBar HueTrackBar1;
+        private System.Windows.Forms.TrackBar HueTrackBar2;
+        private System.Windows.Forms.TrackBar SaturationTrackBar1;
+        private System.Windows.Forms.TrackBar SaturationTrackBar2;
+        private System.Windows.Forms.TrackBar ValueTrackBar1;
+        private System.Windows.Forms.TrackBar ValueTrackBar2;
+    }
+}
---
+++ CountPixel2/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CountPixel2
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        private int _threshold = 150;
+        private int hMin, hMax, sMin, sMax, vMin, vMax = 100;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(1);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            thresholdTrackBar.Value = _threshold;
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+
+                int newHeight = (frame.Size.Height * emguPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(emguPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+                emguPictureBox.Image = frame.Bitmap;
+
+
+                Mat grayFrame = new Mat();
+                Mat binaryFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(grayFrame, binaryFrame, _threshold, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+                binaryPictureBox.Image = binaryFrame.Bitmap;
+
+
+                Image<Gray, byte> binaryImage = binaryFrame.ToImage<Gray, byte>();
+
+                List<int> PixelCount = new List<int>(); //list for the 7 slices
+                int SliceWidth = binaryFrame.Width / 7;
+
+
+                for (int slice = 0; slice < 7; slice++)
+                {
+                    int SliceXcoordinate = SliceWidth * slice;
+                    PixelCount.Add(0);
+                    for (int x = SliceXcoordinate; x < (SliceXcoordinate) + SliceWidth; x++)
+                    {
+                        for (int y = 0; y < binaryImage.Height; y++)
+                        {
+                            if (binaryImage.Data[y, x, 0] == 255)
+                                PixelCount[slice]++;
+                        }
+                    }
+                }
+
+                Invoke(new Action(() =>
+                {
+                    PixelCountLabel1.Text = $"{PixelCount[0]}";
+                    PixelCountLabel2.Text = $"{PixelCount[1]}";
+                    PixelCountLabel3.Text = $"{PixelCount[2]}";
+                    PixelCountLabel4.Text = $"{PixelCount[3]}";
+                    PixelCountLabel5.Text = $"{PixelCount[4]}";
+                    PixelCountLabel6.Text = $"{PixelCount[5]}";
+                    PixelCountLabel7.Text = $"{PixelCount[6]}";
+                }));
+
+                //HSV code
+                Mat HsvFrame = new Mat();
+                CvInvoke.CvtColor(frame, HsvFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+                Mat[] HsvChannels = HsvFrame.Split();
+
+                Mat HueFilter = new Mat();
+                CvInvoke.InRange(HsvChannels[0], new ScalarArray(hMin), new ScalarArray(hMax), HueFilter);
+                Invoke(new Action(() => { HuePictureBox.Image = HueFilter.Bitmap; }));
+
+                Mat SaturationFilter = new Mat();
+                CvInvoke.InRange(HsvChannels[1], new ScalarArray(sMin), new ScalarArray(sMax), SaturationFilter);
+                Invoke(new Action(() => { SaturationPictureBox.Image = SaturationFilter.Bitmap; }));
+
+                Mat ValueFilter = new Mat();
+                CvInvoke.InRange(HsvChannels[2], new ScalarArray(vMin), new ScalarArray(vMax), ValueFilter);
+                Invoke(new Action(() => { ValuePictureBox.Image = ValueFilter.Bitmap; }));
+
+                Mat MergedImage = new Mat();
+                CvInvoke.BitwiseAnd(HueFilter, SaturationFilter, MergedImage);
+                CvInvoke.BitwiseAnd(MergedImage, ValueFilter, MergedImage);
+                Invoke(new Action(() => { RedLinePictureBox.Image = MergedImage.Bitmap; }));
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+        private void thresholdTrackBar_Scroll(object sender, EventArgs e)
+        {
+            _threshold = thresholdTrackBar.Value;
+        }
+
+        private void HueTrackBar1_Scroll(object sender, EventArgs e)
+        {
+            hMin = HueTrackBar1.Value;
+        }
+
+        private void HueTrackBar2_Scroll(object sender, EventArgs e)
+        {
+            hMax = HueTrackBar2.Value;
+        }
+
+        private void SaturationTrackBar1_Scroll(object sender, EventArgs e)
+        {
+            sMin = SaturationTrackBar1.Value;
+        }
+
+        private void SaturationTrackBar2_Scroll(object sender, EventArgs e)
+        {
+            sMax = SaturationTrackBar2.Value;
+        }
+
+        private void ValueTrackBar1_Scroll(object sender, EventArgs e)
+        {
+            vMin = ValueTrackBar1.Value;
+        }
+
+        private void ValueTrackBar2_Scroll(object sender, EventArgs e)
+        {
+            vMax = ValueTrackBar2.Value;
+        }
+    }
+}
